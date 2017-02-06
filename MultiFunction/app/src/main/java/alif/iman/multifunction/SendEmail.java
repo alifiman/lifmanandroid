6d5e836e2477205ad25a761bbd5f96539ef75823
alifiman <lifman.xx@gmail.com>
2017-02-06 12:47:05 +0700

    upload ulang

---
+++ MultiFunction/app/src/main/java/alif/iman/multifunction/SendEmail.java
@@ -0,0 +1,13 @@
+package alif.iman.multifunction;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SendEmail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_email);
+    }
+}
