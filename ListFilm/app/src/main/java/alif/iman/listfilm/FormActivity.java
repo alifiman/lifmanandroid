5e87b3871634d60a935da0bd46fc343de2ce8285
alifiman <lifman.xx@gmail.com>
2017-02-10 10:33:22 +0700

    Daftar Film

---
+++ ListFilm/app/src/main/java/alif/iman/listfilm/FormActivity.java
@@ -0,0 +1,44 @@
+package alif.iman.listfilm;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+public class FormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        String [] rating = {"5.5", "6.0", "6.5", "7.0", "7.5", "8.0", "8.5", "9.0", "9.5"};
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+
+            Spinner spinner = (Spinner) findViewById(R.id.pilihrating);
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                    android.R.layout.simple_spinner_item, rating);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        }
+
+      public void addFile(View view){
+          String title = ((EditText) findViewById(R.id.inpjudul)).getText().toString();
+          int year = Integer.parseInt(((EditText) findViewById(R.id.year)).getText().toString());
+          double rating = Double.parseDouble(((Spinner) findViewById(R.id.pilihrating)).getSelectedItem().toString());
+          String description = ((EditText) findViewById(R.id.description)).getText().toString();
+
+          Movie movie = new Movie(title, description, rating, year);
+
+          Intent intent = new Intent();
+          intent.putExtra("filmlist.result", movie);
+          setResult(Activity.RESULT_OK, intent);
+          finish();
+
+    }
+
+    }
+
--- ListFilm/app/src/main/java/alif/iman/listfilm/MainActivity.java
+++ ListFilm/app/src/main/java/alif/iman/listfilm/MainActivity.java
@@ -1,11 +1,13 @@
 package alif.iman.listfilm;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -15,33 +17,49 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private String[] filmTitles = {"The Thor", "Harry Potter", "Inception",
-                                "Wolf of Wallstreet", "Django Unchained", "Titanic",
-                                "Captain America","Doctor Strange", "X-Men Apocalypse",
-                                "Shutter Island","Bleed For This","The Fault In Our Stars"};
+            "Wolf of Wallstreet", "Django Unchained", "Titanic",
+            "Captain America", "Doctor Strange", "X-Men Apocalypse",
+            "Shutter Island", "Bleed For This", "The Fault In Our Stars"};
 
     private ArrayList<Movie> movies = new ArrayList<>();
-    private void initMovies(){
-        movies.add(new Movie("The Thor","Film tentang dewa yg menjadi superhero dan membawa palu haha",7.5,2009));
-        movies.add(new Movie("Harry Potter","Film tentang penyihir berkacamata bulat",6.5,2007));
-        movies.add(new Movie("Captain America","Film tentang prajurit Amerika yg membawa perisai seperti wajan",7.5,2011));
-        movies.add(new Movie("Titanic","Film tentang cinta ketika Jack & Rose naik kapal bernama Titanic dari Inggris ke America",9.5,1997));
-        movies.add(new Movie("The Fault In Our Stars","Film tentang kisah penderita kanker yg menjalani hidup sebelum selama dia dead",8.5,2013));
-        movies.add(new Movie("Bleed For This","Film tentang petinju yg rela berdarah demi karir tinjunya",8.5,2017));
+
+    ArrayAdapter<Movie> adapter;
+    ListView listView;
+
+    private void initMovies() {
+        movies.add(new Movie("The Thor", "Film tentang dewa yg menjadi superhero dan membawa palu haha", 7.5, 2009));
+        movies.add(new Movie("Harry Potter", "Film tentang penyihir berkacamata bulat", 6.5, 2007));
+        movies.add(new Movie("Captain America", "Film tentang prajurit Amerika yg membawa perisai seperti wajan", 7.5, 2011));
+        movies.add(new Movie("Titanic", "Film tentang cinta ketika Jack & Rose naik kapal bernama Titanic dari Inggris ke America", 9.5, 1997));
+        movies.add(new Movie("The Fault In Our Stars", "Film tentang kisah penderita kanker yg menjalani hidup sebelum selama dia dead", 8.5, 2013));
+        movies.add(new Movie("Bleed For This", "Film tentang petinju yg rela berdarah demi karir tinjunya", 8.5, 2017));
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        initMovies();
         ArrayAdapter<Movie> adapter = new ArrayAdapter<Movie>(this,
                 android.R.layout.simple_list_item_1, movies);
         ListView listView = (ListView) findViewById(R.id.list_film);
         listView.setAdapter(adapter);
+        initMovies();
+        Button btn = (Button) findViewById(R.id.tambah);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+        btn.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+                Intent pindah = new Intent(MainActivity.this,FormActivity.class);
+                startActivity(pindah);
+                //menghubungkan antar activity dengan intent
+
+            }
+        });
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Movie selectedMovie = movies.get(position);
@@ -51,4 +69,25 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    public void TambahListadd(View view){
+        Intent intent = new Intent(this, FormActivity.class);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (requestCode ==1){
+            if (resultCode == Activity.RESULT_OK){
+                Movie newMovie = (Movie) data.getSerializableExtra("filmlist.result");
+                movies.add(newMovie);
+                adapter.notifyDataSetChanged();
+
+            }
+        }
+    }
 }
+
+
+
+
+
